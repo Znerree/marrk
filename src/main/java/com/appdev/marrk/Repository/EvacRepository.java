6dc7f1b1af4c21235a4418b084a587eeac8404d9
Jake Errenz Batulan <jakeerrenz.batulan@cit.edu>
2022-11-18 18:57:19 +0800

    test

--- src/main/java/com/appdev/marrk/Repository/EvacRepository.java
+++ src/main/java/com/appdev/marrk/Repository/EvacRepository.java
@@ -1,9 +1,12 @@
 package com.appdev.marrk.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.appdev.marrk.Entity.EvacEntity;
 
+
+@Repository
 public interface EvacRepository extends JpaRepository<EvacEntity, Integer>{
 	
 }
