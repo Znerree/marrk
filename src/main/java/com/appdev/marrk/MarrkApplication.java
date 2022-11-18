dad7b54c5ceed29acc3290db9f6029e494b49191
Jake Errenz Batulan <jakeerrenz.batulan@cit.edu>
2022-11-18 19:36:16 +0800

    test2

--- src/main/java/com/appdev/marrk/MarrkApplication.java
+++ src/main/java/com/appdev/marrk/MarrkApplication.java
@@ -2,9 +2,8 @@ package com.appdev.marrk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MarrkApplication {
 
 	public static void main(String[] args) {
