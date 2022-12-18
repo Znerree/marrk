bf5a409d1dafebdffd2f0228969be5504762df0d
jeiku14 <jake.prahinog@cit.edu>
2022-12-18 14:51:03 +0800

    cross-origins error

--- src/main/java/com/appdev/marrk/Controller/CitizenController.java
+++ src/main/java/com/appdev/marrk/Controller/CitizenController.java
@@ -3,6 +3,7 @@ package com.appdev.marrk.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.appdev.marrk.Entity.CitizenEntity;
 import com.appdev.marrk.Service.CitizenService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/citizen")
 public class CitizenController {
 	
--- src/main/java/com/appdev/marrk/Controller/ContactController.java
+++ src/main/java/com/appdev/marrk/Controller/ContactController.java
@@ -3,6 +3,7 @@ package com.appdev.marrk.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.appdev.marrk.Entity.ContactEntity;
 import com.appdev.marrk.Service.ContactService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/contacts")
 public class ContactController {
 	
