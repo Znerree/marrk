ea59d1e78893b7099125e3bec613503571c7af3c
jeiku14 <jake.prahinog@cit.edu>
2022-12-21 15:24:55 +0800

    cross origin

--- src/main/java/com/appdev/marrk/Controller/EvacController.java
+++ src/main/java/com/appdev/marrk/Controller/EvacController.java
@@ -3,6 +3,7 @@ package com.appdev.marrk.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.appdev.marrk.Service.EvacService;
 
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/evac")
 public class EvacController {
 	
